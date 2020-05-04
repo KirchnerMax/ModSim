9a3035d1b4dc1d6daac2d4986c3ec1dd6f40c6b6
KirchnerMax <kirchnermax98@googlemail.com>
2020-05-04 22:19:42 +0200

    Aufgabe 2 - kleine Änderung

--- main_a1.m
+++ main_a1.m
@@ -6,7 +6,8 @@
 % zu erg?nzende Codezeilen sind mit ">>> erg?nzen ...." und ...¡°gekennzeichnet
 clear all % L?sche Arbeitsspeicher
 Tm = 10; % Konstante des PT1, [s]
-h = 0.1; % Schrittweite, (s)
+h = [];
+h(1)=0.1;% Schrittweite, (s)
 t0 = 0; % Integrationsbeginn, [s]
 tf = 300; % Integrationsende, [s]
 t = []; % Zeitwerte f¨¹r Plot [s]
@@ -14,6 +15,7 @@ d = []; % Fehler-Sch?tzwerte
 u = []; % Stellwerte u(t)
 y = []; % Ausgangswerte y(t)
 ys = []; % Soll-Ausgangswerte y_soll(t)
+to = 5*10^(-5); %Toleranz LDF
 % Initialisierung
 [dum,x(1)] = system_pt1([],[],[],0);
 d(1) = 0;
@@ -21,6 +23,7 @@ d(1) = 0;
 ti = t0;
 i = 1;
 while ti <= tf
+
     % Berechnung des Soll-Ausgangswertes
     ys(i) = x(i);
     % Berechnung des Stellwertes
@@ -29,17 +32,28 @@ while ti <= tf
     y(i) = system_pt1(ti ,x(i) ,u(i) , 3); %die Parameter einsetzen
     % Berechnung der Koeffizienten f¨¹r VPG-Methode
     k1 = system_pt1( ti ,x(i) ,u(i) , 1); %die Parameter einsetzen
-    k2 = system_pt1( ti+h/2 , x(i)+h/2*k1 ,u(i) , 1); %die Parameter einsetzen
-    k3 = system_pt1(ti+h , x(i)-h*k1+2*h*k2 ,u(i) , 1); %die Parameter einsetzen
+    k2 = system_pt1( ti+h(i)/2 , x(i)+h(i)/2*k1 ,u(i) , 1); %die Parameter einsetzen
+    k3 = system_pt1(ti+h(i) , x(i)-h(i)*k1+2*h(i)*k2 ,u(i) , 1); %die Parameter einsetzen
     % Wichtiger Hinweis: Die Parameter bei den Aufrufen von system_pt1(...)
     % m¨¹ssen unter Beachtung von jeweiligen Zeitpunkten bestimmt werden!
     % Berechnung des Zustands-Sch?tzwertes x(ti+h)
-    x(i+1) = x(i)+h/6*(k1+4*k2+k3);
+    x(i+1) = x(i)+h(i)*k2;
     % Berechnung der LDF Fehlerabsch?tzung d(ti+h)
-    d(i+1) = h/6*(k1-2*k2+k3);
-    t(i) = ti; % Zeitwert f¨¹r Plot speichern
-    ti = ti + h; % Zeitvariable um einen Schritt erh?hen
-    i = i + 1; % Index inkrementieren
+    d(i+1) = h(i)/6*(k1-2*k2+k3);
+    hn = h(i)*(to/max(abs(d(i+1))))^(1/3);
+    if hn>2*h(i)&&hn<20
+        h(i+1) = hn;
+        t(i) = ti; % Zeitwert f¨¹r Plot speichern
+        ti = ti + h(i); % Zeitvariable um einen Schritt erh?hen
+        i = i + 1; % Index inkrementieren
+    elseif (hn>0)&&(hn<=h(i))
+        h(i) = 0.75*hn;
+    else 
+        h(i+1)=h(i);
+        t(i) = ti; % Zeitwert f¨¹r Plot speichern
+        ti = ti + h(i); % Zeitvariable um einen Schritt erh?hen
+        i = i + 1; % Index inkrementieren
+    end
 end
 d = d(1:end-1);
 result = [t;d];
@@ -53,3 +67,6 @@ subplot(2,1,1); plot(t,y-ys,'.-'); title('GDF berechnet');zoom on;grid on;
 tit=sprintf('LDF gesch?tzt: max. Betrag = %g',max(abs(d)));
 subplot(2,1,2); plot(t,d,'.-'); title(tit);zoom on;grid on;
 xlabel('Zeit, s');
+figure(3);
+plot(t,h(1:133));title('Schrittweite');zoom on;grid on;
+xlabel('Zeit,s')
