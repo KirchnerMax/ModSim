b34ce32c1667f237755bee84a278ac8079437de1
KleistvonLiu <61907235+KleistvonLiu@users.noreply.github.com>
2020-05-11 12:34:26 +0200

    Update VPG.m

--- VPG.m
+++ VPG.m
@@ -1,11 +1,13 @@
-function [x_VPG,y,hn] = VPG(model_name,x,u,t,h,yp,i)
-to = 5*10^(-8); %Toleranz LDF
+function [x_VPG,y,hn] = VPG(model_name,x,t,h,yp,i)
+global to
 global d
 % derivatives @ t, t+h/2, t+h
 [k1, y]   = feval(model_name, x,        t,yp     );
-[k2, dum] = sys1(x+h(i)/2*k1,y(1),t+h(i)/2);
-[k3, dum] = sys1(x+h(i)*2*k2-h(i)*k1,y(1),t+h(i));
-% RK4 integration step
+%[k2, dum] = sys1(x+h(i)/2*k1,y(1),t+h(i)/2);
+%[k3, dum] = sys1(x+h(i)*2*k2-h(i)*k1,y(1),t+h(i));
+[k2, dum] = feval(model_name, x+h(i)/2*k1, t+h(i)/2,yp );
+[k3, dum] = feval(model_name, x+h(i)*2*k2-h(i)*k1, t+h(i),yp);
+% VPG integration step
 x_VPG = x + h(i)*k2;
 d(i+1) = h(i)/6*(k1-2*k2+k3);
 hn = h(i)*(to/max(abs(d(i+1))))^(1/3);
